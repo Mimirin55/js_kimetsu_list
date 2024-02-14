110346233a59265f1a31fd181848578de7ce0ba2
Mimirin55 <178504.hinotori@gmail.com>
2024-02-14 13:18:17 +0900

    API取得・表示メソッド修正

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const container = document.getElementById('kimetsu_container');
 document.getElementById("loading").style.display ="none";
 
-// 全キャラクター
-async function allApi() {
-  const res = await fetch("https://ihatov08.github.io/kimetsu_api/api/all.json");
+async function callApi(value) {
+  if (value.value == "all") {
+    url = "https://ihatov08.github.io/kimetsu_api/api/all.json";
+  } else if (value.value == "kisatsutai") {
+    url = "https://ihatov08.github.io/kimetsu_api/api/kisatsutai.json";
+  } else if (value.value == "hashira") {
+    url = "https://ihatov08.github.io/kimetsu_api/api/hashira.json";
+  } else {
+    url = "https://ihatov08.github.io/kimetsu_api/api/oni.json";
+  };
+
+  const res = await fetch(url);
   const characters = await res.json();
 
   for(let i = 0; i < characters.length; i++) {
@@ -15,63 +24,11 @@ async function allApi() {
         <div class="card-subtitle">【${characters[i].category}】</div>
       </div>
     `;
-  }
+  };
 };
 
-allApi();
+callApi(all);
 
-// 鬼殺隊
-async function kisatsutaiApi() {
-  const res = await fetch("https://ihatov08.github.io/kimetsu_api/api/kisatsutai.json");
-  const characters = await res.json();
-
-  for(let i = 0; i < characters.length; i++) {
-    container.innerHTML += `
-      <link rel="stylesheet" href="styles.css">
-      <div class="card">
-        <div class="card-title">${characters[i].name}</div>
-        <img src="https://ihatov08.github.io${characters[i].image}" class="card-image">
-        <div class="card-subtitle">【${characters[i].category}】</div>
-      </div>
-    `;
-  }
-}
-
-// 柱
-async function hashiraApi() {
-  const res = await fetch("https://ihatov08.github.io/kimetsu_api/api/hashira.json");
-  const characters = await res.json();
-
-  for(let i = 0; i < characters.length; i++) {
-    container.innerHTML += `
-      <link rel="stylesheet" href="styles.css">
-      <div class="card">
-        <div class="card-title">${characters[i].name}</div>
-        <img src="https://ihatov08.github.io${characters[i].image}" class="card-image">
-        <div class="card-subtitle">【${characters[i].category}】</div>
-      </div>
-    `;
-  }
-};
-
-// 鬼
-async function oniApi() {
-  const res = await fetch("https://ihatov08.github.io/kimetsu_api/api/oni.json");
-  const characters = await res.json();
-
-  for(let i = 0; i < characters.length; i++) {
-    container.innerHTML += `
-      <link rel="stylesheet" href="styles.css">
-        <div class="card">
-        <div class="card-title">${characters[i].name}</div>
-        <img src="https://ihatov08.github.io${characters[i].image}" class="card-image">
-        <div class="card-subtitle">【${characters[i].category}】</div>
-      </div>
-    `;
-  }
-};
-
-// キャラクター選択時のAPI読み込み
 function check_form() {
   let value = document.form.character;
   
@@ -80,48 +37,22 @@ function check_form() {
       loading.style.display = 'block';
       container.innerHTML = '';
 
-      switch(value[i].value) {
-        case "all":
-        new Promise((resolve) => {
-          setTimeout(() => {
-            loading.style.display = 'none';
-            resolve();
-          }, 1000);
-        }).then(() => {
-          allApi();
-        });
-        break;
-        case "kisatsutai":
-          new Promise((resolve) => {
-            setTimeout(() => {
-              loading.style.display = 'none';
-              resolve();
-            }, 1000);
-          }).then(() => {
-            kisatsutaiApi();
-          });
-        break;
-        case "hashira":
-          new Promise((resolve) => {
-            setTimeout(() => {
-              loading.style.display = 'none';
-              resolve();
-            }, 1000);
-          }).then(() => {
-            hashiraApi();
-          });
-        break;
-        case "oni":
-          new Promise((resolve) => {
-            setTimeout(() => {
-              loading.style.display = 'none';
-              resolve();
-            }, 1000);
-          }).then(() => {
-            oniApi();
-          });
-        break;
-      };
+      new Promise((resolve) => {
+        setTimeout(() => {
+          loading.style.display = 'none';
+          resolve();
+        }, 1000);
+      }).then(() => {
+        if (value.value == "all") {
+          callApi(all);
+        } else if (value.value == "kisatsutai") {
+          callApi(kisatsutai);
+        } else if (value.value == "hashira") {
+          callApi(hashira);
+        } else {
+          callApi(oni);
+        }
+      })
     };
   };
 };
