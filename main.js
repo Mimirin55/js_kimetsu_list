125c0e927591b7a65c1dcd4c9bf3c9013e6e7154
Mimirin55 <178504.hinotori@gmail.com>
2024-02-15 20:18:19 +0900

    条件分岐部分修正

--- main.js
+++ main.js
@@ -2,17 +2,7 @@ const container = document.getElementById('kimetsu_container');
 document.getElementById("loading").style.display ="none";
 
 async function callApi(value) {
-  if (value.value == "all") {
-    url = "https://ihatov08.github.io/kimetsu_api/api/all.json";
-  } else if (value.value == "kisatsutai") {
-    url = "https://ihatov08.github.io/kimetsu_api/api/kisatsutai.json";
-  } else if (value.value == "hashira") {
-    url = "https://ihatov08.github.io/kimetsu_api/api/hashira.json";
-  } else {
-    url = "https://ihatov08.github.io/kimetsu_api/api/oni.json";
-  };
-
-  const res = await fetch(url);
+  const res = await fetch(`https://ihatov08.github.io/kimetsu_api/api/${value.value}.json`);
   const characters = await res.json();
 
   for(let i = 0; i < characters.length; i++) {
@@ -43,15 +33,7 @@ function check_form() {
           resolve();
         }, 1000);
       }).then(() => {
-        if (value.value == "all") {
-          callApi(all);
-        } else if (value.value == "kisatsutai") {
-          callApi(kisatsutai);
-        } else if (value.value == "hashira") {
-          callApi(hashira);
-        } else {
-          callApi(oni);
-        }
+        callApi(value);
       })
     };
   };
